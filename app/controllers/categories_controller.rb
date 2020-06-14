65cd097f9889fb334f75a5b29f41c64a3d48a793
Siyana Plachkova <siyanaplachkova@gmail.com>
2020-06-14 23:00:03 +0300

    Add new and edit actions. Add route. Implement form.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,12 @@
 class CategoriesController < ApplicationController
 
   def index
+  end
 
+  def new
+  end
+
+  def edit
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'categories', to: 'categories#index'
+  get 'categories/new', to: 'categories#new'
 
 end
