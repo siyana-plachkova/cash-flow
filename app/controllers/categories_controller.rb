829fedba98295a9823f84cb86b5d89317cffb5ab
Siyana Plachkova <siyanaplachkova@gmail.com>
2020-09-19 22:11:59 +0300

    Add new expense modal

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,4 @@ class CategoriesController < ApplicationController
   def index
   end
 
-  def new
-  end
-
-  def edit
-  end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
